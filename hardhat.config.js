7822d0e59e73ca114936cf8f84635e890c4d61c0
sonimwangi github <sonimwangi@gmail.com>
2024-11-16 14:56:28 +0300

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,39 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env" });
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY_SEPOLIA}`,
+      accounts: [process.env.WALLET_PRIVATE_KEY],
+    },
+
+    ethereum: {
+      url: `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY_MAIN}`,
+      accounts: [process.env.WALLET_PRIVATE_KEY],
+    },
+
+    basesepolia: {
+      url: `https://base-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY_SEPOLIA}`,
+      accounts: [process.env.WALLET_PRIVATE_KEY],
+    },
+
+    base: {
+      url: `https://base-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY_MAIN}`,
+      accounts: [process.env.WALLET_PRIVATE_KEY],
+    },
+
+
+
+
+  },
+
+
+
+
+
+
+  solidity: "0.8.27",
+};
